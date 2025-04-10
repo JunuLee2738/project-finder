e63d138a4ae3861309ff34a3a62caeef864be2c1
GenerativeAI-openai <ijunu3343@gmail.com>
2025-04-10 17:40:02 +0900

    Update entry.js

--- api/entry.js
+++ api/entry.js
@@ -1,4 +1,4 @@
-const https = require("https");
+const fetch = require("node-fetch");
 
 module.exports = async (req, res) => {
   const { id } = req.query;
@@ -9,210 +9,48 @@ module.exports = async (req, res) => {
 
   const body = {
     query: `
-
-    query SELECT_PROJECT($id: ID! $groupId: ID) {
-        project(id: $id, groupId: $groupId) {     
-    
-    id
-    name
-    user {
-        
-    id
-    nickname
-    profileImage {
-        
-    id
-    name
-    label {
-        
-    ko
-    en
-    ja
-    vn
-
-    }
-    filename
-    imageType
-    dimension {
-        
-    width
-    height
-
-    }
-    trimmed {
-        filename
-        width
-        height
-    }
-
-    }
-    status {
-        following
-        follower
-    }
-    description
-    role
-    mark {
-        
-    id
-    name
-    label {
-        
-    ko
-    en
-    ja
-    vn
-
-    }
-    filename
-    imageType
-    dimension {
-        
-    width
-    height
-
-    }
-    trimmed {
-        filename
-        width
-        height
-    }
- 
-    }
-
-    }
-    thumb
-    isopen
-    showComment
-    blamed
-    isPracticalCourse
-    category
-    categoryCode
-    created
-    updated
-    special
-    isForLecture
-    isForStudy
-    isForSubmit
-    hashId
-    complexity
-    staffPicked
-    ranked
-    visit
-    likeCnt
-    comment
-    favorite
-    shortenUrl
-    parent {
-        id
-        name
-        user {
-            id
-            nickname
+      query SELECT_PROJECT($id: ID! $groupId: ID) {
+        project(id: $id, groupId: $groupId) {
+          id name user { id nickname profileImage {
+            id name label { ko en ja vn } filename imageType dimension { width height } trimmed { filename width height }
+          } status { following follower } description role mark {
+            id name label { ko en ja vn } filename imageType dimension { width height } trimmed { filename width height }
+          }}
+          thumb isopen showComment blamed isPracticalCourse category categoryCode created updated special
+          isForLecture isForStudy isForSubmit hashId complexity staffPicked ranked visit likeCnt comment favorite
+          shortenUrl parent { id name user { id nickname } }
+          description description2 description3 hasRealTimeVariable blockCategoryUsage childCnt
+          commentGroup { group count } likeCntGroup { group count } visitGroup { group count } recentGroup { group count }
+          published isFirstPublish tags
+          speed objects variables submitId { id } cloudVariable messages functions tables scenes
+          realTimeVariable {
+            variableType key value array { key data } minValue maxValue visible x y width height object
+          }
+          learning expansionBlocks aiUtilizeBlocks hardwareLiteBlocks blockCategoryUsage
         }
-    }
-    description
-    description2
-    description3
-    hasRealTimeVariable
-    blockCategoryUsage
-    childCnt
-    commentGroup {
-        group
-        count
-    }
-    likeCntGroup {
-        group
-        count
-    }
-    visitGroup {
-        group
-        count
-    }
-    recentGroup {
-        group
-        count
-    }
-    published
-    isFirstPublish
-    tags
-
-    speed
-    objects
-    variables
-    submitId {
-        id
-    }
-    cloudVariable
-    messages
-    functions
-    tables
-    scenes
-    realTimeVariable {
-        
-    variableType
-    key
-    value
-    array {
-        key
-        data
-    }
-    minValue
-    maxValue
-    visible
-    x
-    y
-    width
-    height
-    object
-
-    }
-    learning
-    expansionBlocks
-    aiUtilizeBlocks
-    hardwareLiteBlocks
-    blockCategoryUsage
-
-        }
-     }`,
+      }
+    `,
     variables: { id }
   };
 
-  const requestBody = JSON.stringify(body);
-//      "Content-Length": Buffer.byteLength(requestBody),
-  const options = {
-    hostname: "playentry.org",
-    path: "playentry.org/graphql/SELECT_PROJECT",
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Content-Length": Buffer.byteLength(requestBody),
-      "Origin": "https://playentry.org"
+  try {
+    const response = await fetch("https://playentry.org/graphql/SELECT_PROJECT", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Origin": "https://playentry.org"
+      },
+      body: JSON.stringify(body)
+    });
+
+    if (!response.ok) {
+      return res.status(500).json({ error: "Entry server error", status: response.status });
     }
-  };
 
-  const request = https.request(options, (response) => {
-    let data = "";
-
-    response.on("data", (chunk) => {
-      data += chunk;
-    });
-
-    response.on("end", () => {
-      try {
-        const parsed = JSON.parse(requestBody);
-        //res.setHeader("Access-Control-Allow-Origin", "*");
-        res.status(200).json(parsed);
-      } catch (err) {
-        res.status(500).json({ error: "JSON parse error", detail: err.message });
-      }
-    });
-  });
-
-  request.on("error", (err) => {
-    res.status(500).json({ error: "Request error", detail: err.message });
-  });
-
-  request.write(requestBody);
-  request.end();
+    const data = await response.json();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.status(500).json({ error: "Internal error", detail: err.message });
+  }
 };
