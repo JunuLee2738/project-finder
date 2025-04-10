1f38bb4ba5c16c38d6d5e4ca28de061daf59bfb4
GenerativeAI-openai <ijunu3343@gmail.com>
2025-04-10 17:32:18 +0900

    Update entry.js

--- api/entry.js
+++ api/entry.js
@@ -179,14 +179,14 @@ module.exports = async (req, res) => {
   };
 
   const requestBody = JSON.stringify(body);
-//      "Content-Length": Buffer.byteLength(body),
+//      "Content-Length": Buffer.byteLength(requestBody),
   const options = {
     hostname: "playentry.org",
     path: "/graphql/SELECT_PROJECT",
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Content-Length": Buffer.byteLength(body),
+      "Content-Length": Buffer.byteLength(requestBody),
       "Origin": "https://playentry.org"
     }
   };
