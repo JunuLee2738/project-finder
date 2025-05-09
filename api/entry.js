82f689ec0caefb4ffad1252cbb29ea3cf4d691d4
GenerativeAI-openai <ijunu3343@gmail.com>
2025-05-09 20:20:14 +0900

    Update entry.js

--- api/entry.js
+++ api/entry.js
@@ -2,33 +2,173 @@ export default async function handler(req, res) {
   const { id, groupId = null } = req.query;
 
   const query = `
-    query SELECT_PROJECT($id: ID!, $groupId: ID) {
-      project(id: $id, groupId: $groupId) {
-        id name user {
-          id nickname profileImage {
-            id name label { ko en ja vn }
-            filename imageType dimension { width height }
-            trimmed { filename width height }
-          }
-          status { following follower } description role mark {
-            id name label { ko en ja vn }
-            filename imageType dimension { width height }
-            trimmed { filename width height }
-          }
-        }
-        thumb isopen showComment blamed isPracticalCourse category categoryCode created updated special
-        isForLecture isForStudy isForSubmit hashId complexity staffPicked ranked visit likeCnt comment favorite
-        shortenUrl parent { id name user { id nickname } }
-        description description2 description3 hasRealTimeVariable blockCategoryUsage childCnt
-        commentGroup { group count } likeCntGroup { group count } visitGroup { group count } recentGroup { group count }
-        published isFirstPublish tags
-        speed objects variables submitId { id } cloudVariable messages functions tables scenes
-        realTimeVariable {
-          variableType key value array { key data } minValue maxValue visible x y width height object
-        }
-        learning expansionBlocks aiUtilizeBlocks hardwareLiteBlocks blockCategoryUsage
-      }
+
+    query SELECT_PROJECT($id: ID! $groupId: ID) {
+        project(id: $id, groupId: $groupId) {
+            
+    
+    id
+    name
+    user {
+        
+    id
+    nickname
+    profileImage {
+        
+    id
+    name
+    label {
+        
+    ko
+    en
+    ja
+    vn
+
     }
+    filename
+    imageType
+    dimension {
+        
+    width
+    height
+
+    }
+    trimmed {
+        filename
+        width
+        height
+    }
+
+    }
+    status {
+        following
+        follower
+    }
+    description
+    role
+    mark {
+        
+    id
+    name
+    label {
+        
+    ko
+    en
+    ja
+    vn
+
+    }
+    filename
+    imageType
+    dimension {
+        
+    width
+    height
+
+    }
+    trimmed {
+        filename
+        width
+        height
+    }
+ 
+    }
+
+    }
+    thumb
+    isopen
+    showComment
+    blamed
+    isPracticalCourse
+    category
+    categoryCode
+    created
+    updated
+    special
+    isForLecture
+    isForStudy
+    isForSubmit
+    hashId
+    complexity
+    staffPicked
+    ranked
+    visit
+    likeCnt
+    comment
+    favorite
+    shortenUrl
+    parent {
+        id
+        name
+        user {
+            id
+            nickname
+        }
+    }
+    description
+    description2
+    description3
+    hasRealTimeVariable
+    blockCategoryUsage
+    childCnt
+    commentGroup {
+        group
+        count
+    }
+    likeCntGroup {
+        group
+        count
+    }
+    visitGroup {
+        group
+        count
+    }
+    recentGroup {
+        group
+        count
+    }
+    published
+    isFirstPublish
+    tags
+
+    speed
+    objects
+    variables
+    submitId {
+        id
+    }
+    cloudVariable
+    messages
+    functions
+    tables
+    scenes
+    realTimeVariable {
+        
+    variableType
+    key
+    value
+    array {
+        key
+        data
+    }
+    minValue
+    maxValue
+    visible
+    x
+    y
+    width
+    height
+    object
+
+    }
+    learning
+    expansionBlocks
+    aiUtilizeBlocks
+    hardwareLiteBlocks
+    blockCategoryUsage
+
+        }
+     }
   `;
 
   try {
@@ -47,7 +187,6 @@ export default async function handler(req, res) {
         query,
         variables: {
           id,
-          groupId,
         },
       }),
     });
