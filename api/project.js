f348d187e2a362c2fc05502ed5078e40c0884d97
GenerativeAI-openai <ijunu3343@gmail.com>
2025-06-11 20:27:58 +0900

    Update project.js

--- api/project.js
+++ api/project.js
@@ -517,7 +517,7 @@ export default async function handler(req, res) {
         "x-client-type": "Client",
         "Referer": "https://playentry.org/",
         "Origin": "https://playentry.org",
-        "x-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTgzZDE5ODRhNjAwZDAwMjU1ZTc4YmMiLCJwcm9qZWN0SWQiOiI2ODQxMzhkOTYyYzM5NjgyODYyZDA0NWQiLCJwcm9qZWN0VXNlcklkIjoiNjU4M2QxOTg0YTYwMGQwMDI1NWU3OGJjIiwiaXNPd25lciI6dHJ1ZSwiaWF0IjoxNzQ5MTA1MDA0fQ.zDdtB44MBrKMoN8LPPYKpz6OkQuqMheGwyd2qhHfYNU",
+        "x-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTgzZDE5ODRhNjAwZDAwMjU1ZTc4YmMiLCJwcm9qZWN0SWQiOiI2N2RlOGMwYzI0ZGM4MzliYTZlMTYzNDUiLCJwcm9qZWN0VXNlcklkIjoiNjZiZWU2YzlhZjZhYzBhYjEyZGY2OTRmIiwiaXNPd25lciI6ZmFsc2UsImlhdCI6MTc0OTY0MTI0OX0.V3LsEFETSIhLubFZPKvp4oJ-KFEhS9vLxT6hG23lsZA",
         "remote-address": "110.93.151.161:443",
         "csrf-token": "UPGcF8Xg-sB1a8MiBJtk51znd_HbyRv7mU8E",
       },
