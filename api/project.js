255ab4c26356f56f5aba373e7351e04b558a30aa
JunuLee2738 <ijunu2738@gmail.com>
2025-07-09 20:17:14 +0900

    Update project.js

--- api/project.js
+++ api/project.js
@@ -581,6 +581,7 @@ export default async function handler(req, res) {
             "x-client-type": "Client",
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
             "remote-address": "110.93.151.161:443",
+            "origin": "https://playentry.org/"
             //"x-token": xToken
         },
         body: JSON.stringify({
